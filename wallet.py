9898db00774b2716b78e9491701af485074b5a58
Hassan Ashfaq <60746056+Hassan-Ashfaq@users.noreply.github.com>
2023-02-05 16:06:20 +0500

    Create wallet.py

---
+++ wallet.py
@@ -0,0 +1,15 @@
+class Wallet:
+  def __init__(self):
+    self.balance = 0
+   
+  def depositMoney(self, money):
+    self.balance = money
+  
+  def queryMoney(self):
+    return self.balance
+  
+  def withdrawal(self, money):
+    if self.balance >= money:
+      self.balance -= money
+      return 'Done'
+    return 'Low Balance'
