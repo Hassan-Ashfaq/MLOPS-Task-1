8374a76f0bd327d959fdcd7a650013ae70967601
Hassan Ashfaq <60746056+Hassan-Ashfaq@users.noreply.github.com>
2023-02-05 16:08:29 +0500

    Create test.py

---
+++ test.py
@@ -0,0 +1,16 @@
+from wallet import Wallet
+
+def test_getAmount():
+  a = Wallet()
+  a.depositMoney(1000)
+  assert a.queryMoney()==1000
+  
+def test_removeAmount_pass():
+  a = Wallet()
+  a.depositMoney(1000)
+  assert a.withdrawal(100)=='Done'
+
+def test_removeAmount_low():
+  a = Wallet()
+  a.depositMoney(1000)
+  assert a.withdrawal(2000)=='Low Balance'
